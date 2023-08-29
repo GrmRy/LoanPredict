e696eab6e32f87de7691860f6a103c241c878114
Andrea Satria Nagari <93112763+GrmRy@users.noreply.github.com>
2023-08-29 15:04:29 +0000

    add script to predict the model using FastApi

---
+++ backend.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+import joblib
+
+#load model
+model=joblib.load("/workspaces/LoanPredict/loan_classifier.joblib")
+
+app=FastAPI()
+
+class Input(BaseModel):
+    
